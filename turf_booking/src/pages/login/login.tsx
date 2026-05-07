c644f3382a563be5781b2089b2c1c172d84bd552
amarthyagp002 <amarthyagp2002@gmail.com>
2026-05-07 17:08:32 +0530

    created login and register pages

---
+++ turf_booking/src/pages/login/login.tsx
@@ -0,0 +1,41 @@
+{/* <html>
+    <head>
+        <link rel="stylesheet" href="./login_page.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="login-outer-container">
+            <div class="login-inner-container">
+                <div class="login-forms">
+                <h1 >Login</h1>
+                <form action="/login" method="POST" class="login-inner-form">
+                    <input type="text" placeholder="jhon@gmail.com" required>
+                    <input type="password" placeholder="******" required>
+                    <a class="login-forgot" href="register_page.html">forgot password?</a>
+                    <button type="submit">Login</button>
+                    <div class="login-register-link">
+                       Don't have an account? <a href="register_page.html">Register</a>
+                    </div>
+                    
+                </form>
+                </div>
+                <div class="login-down">
+                <img src="./pana.png" class="login-image">
+            </div>
+                
+            </div>
+            
+        </div>
+        
+    </body>
+</html> */}
+function Login(){
+return(
+    <div>
+        <p>
+            amar
+        </p>
+    </div>
+)
+}
+export default Login;
---
+++ turf_booking/src/pages/register/register.tsx
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="./register_page.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="register-outer-container">
+            <div class="register-inner-container">
+                <div class="register-forms">
+                <h1 >Login</h1>
+                <form action="/register" method="POST" class="register-inner-form">
+                    <input type="text" placeholder="jhon@gmail.com" required>
+                    <a class="register-forgot" href="register_page.html">forgot password?</a>
+                    <button type="submit">Register</button>
+                    <div class="register-register-link">
+                       Already a member? <a href="login_page.html">Login</a>
+                    </div>
+                    
+                </form>
+                </div>
+                <div class="register-down">
+                <img src="./pana.png" class="register-image">
+            </div>
+                
+            </div>
+            
+        </div>
+        
+    </body>
+</html>
