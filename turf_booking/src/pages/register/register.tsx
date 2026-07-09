569ab163e839f43fc78d8a1341ea5dbe604efa0b
Brijithk <keranbrijith@gmail.com>
2026-07-09 10:27:18 +0530

    added form feature

--- turf_booking/src/pages/register/register.tsx
+++ turf_booking/src/pages/register/register.tsx
@@ -1,41 +1,52 @@
 import { useNavigate } from "react-router-dom"
 import "../register/register.css"
 import registerImage from "../../assets/pana.png"
-import { useState } from "react"
-function register(){
+import React, { useState } from "react";
+interface FormState{
+    email:string;
+    password:string;
+}
+function register() {
     const navigate = useNavigate()
-    const [regData,setRegData]=useState({email:'',password:''})
+    const [regData, setRegData] = useState<FormState>({
+        email: '',
+        password: ''
+    })
     // const regData=[]
-    const handleRegister=(e:any)=>{
-        const {name:value}=e.target.value
-        set1
-        
-           alert("enter the otp in next page");
-           navigate('/otp')
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setRegData((prev)=>({...prev,[name]:value}))
+
+        alert("enter the otp in next page");
+        navigate('/otp')
+    }
+    const handleSubmit=(e:React.SyntheticEvent<HTMLFormElement>)=>{
+        e.preventDefault();
     }
     return <>
-            <div className="register-outer-container">
+        <div className="register-outer-container">
             <div className="register-inner-container">
                 <div className="register-forms">
-                <h1 >Register</h1>
-                <div className="register-inner-form">
-                    <input type="text" name='email' placeholder="jhon@gmail.com" required></input>
-                    <a className="register-forgot" href="register_page.html">forgot password?</a>
-                    <button type="submit" onClick={handleRegister}>Register</button>
-                    <div className="register-register-link">
-                       Already a member? <a>Login</a>
-                    </div>
-                    
-                </div>
+                    <h1 >Register</h1>
+                    <form onSubmit={handleSubmit} className="register-inner-form">
+                        <input type="text" name='email' value={regData.email} onChange={handleChange} placeholder="jhon@gmail.com" required></input>
+                        <input type="password" name='password' value={regData.password} onChange={handleChange} placeholder="jhon@gmail.com" required></input>
+                        <a className="register-forgot" href="register_page.html">forgot password?</a>
+                        <button type="submit">Register</button>
+                        <div className="register-register-link">
+                            Already a member? <a>Login</a>
+                        </div>
+
+                    </form>
                 </div>
                 <div className="register-down">
-                <img src={registerImage} className="register-image"></img>
+                    <img src={registerImage} className="register-image"></img>
+                </div>
+
             </div>
-                
-            </div>
-            
+
         </div>
-    
+
     </>
 }
 export default register
